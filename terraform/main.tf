8325baedab6f4756e48171976351ae3f3c472e05
Gabriel Andrade <46279433+gabrieldsinfo@users.noreply.github.com>
2025-07-17 15:08:26 -0300

    Update main.tf
    Ajuste variavel kubeconfig_path
    correcao kubeconfig

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,11 +11,18 @@ terraform {
   }
 }
 
+variable "kubeconfig_path" {
+  type    = string
+  default = "~/.kube/config" 
+}
+
 provider "kubernetes" {
+  config_path = var.kubeconfig_path
 }
 
 provider "helm" {
   kubernetes {
+    config_path = var.kubeconfig_path
   }
 }
 
